075ac0f37e654549ec6bdd007d93c68bbc79f53f
zhazha <zhazha@qq.com>
2022-03-29 21:19:13 +0800

    事件

--- pages/index1/index1.js
+++ pages/index1/index1.js
@@ -1,9 +1,21 @@
-// pages/index1/index1.js
-Page({
 
-    /**
-     * 页面的初始数据
-     */
-    data: {
+Page({
+    onLoad: function (option) {
+        console.log(option.query)
+        const eventChannel = this.getOpenerEventChannel()
+        eventChannel.emit('acceptDataFromOpenedPage', { data: 'test' });
+        eventChannel.emit('someEvent', { data: 'test' });
+        // 监听acceptDataFromOpenerPage事件，获取上一页面通过eventChannel传送到当前页面的数据
+        eventChannel.on('acceptDataFromOpenerPage', function (data) {
+            console.log(data)
+        })
     },
+
+
+    toPostPage() {
+        wx.navigateTo({
+            url: '/pages/posts/posts',
+        })
+    }
+
 })

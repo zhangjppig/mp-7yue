fed9992ea3762e75811582111106862222aa9336
zhazha <zhazha@qq.com>
2022-03-31 20:36:36 +0800

    js模块的导入导出

---
+++ data/data.js
@@ -0,0 +1,73 @@
+let postList=[
+  {
+  title:"2020观看指南",
+  content:" 这里有一大段文字,小程序提供了一个简单、高效的应用开发框架和丰富的组件及API，帮助开发者在微信中开发具有原生 APP 体验的服务。本章分主题的介绍了小程序的开发语言、框架、能力、调试等内容，帮助开发者快速全面的了解小程序开发的方方面面。",
+  avatar:"/images/2.png",
+  reading:133,
+  collection:22,
+  author:"猫吃鱼",
+  detail:"12312ee",
+  dateTime:"24小时前",
+  date:"Nov 12 2020",
+  headImgsrc:"/images/1.png",
+  imgSrc:"/images/1.jpg",
+},
+{ date:"Sep 18 2020",
+  title:"大好河山",
+  imgSrc:"/images/2.jpg",
+  avatar:"/images/2.png",
+  content:"菊黄蟹正肥，巴拉巴拉巴拉……",
+  reading:"112",
+  collection:"96",
+  headImgsrc:"/images/2.jpg",
+  author:"黑衣人",
+  dateTime:"24小时前",
+},
+{ date:"Sep 18 2020",
+  title:"大6666",
+  imgSrc:"/images/2.jpg",
+  avatar:"/images/2.png",
+  content:"菊黄蟹正肥，巴拉巴拉巴拉……",
+  reading:"112",
+  collection:"96",
+  headImgsrc:"/images/2.jpg",
+  author:"黑衣人",
+  dateTime:"24小时前",
+},
+{ date:"Sep 18 2020",
+  title:"7788",
+  imgSrc:"/images/2.jpg",
+  avatar:"/images/2.png",
+  content:"菊黄蟹正肥，巴拉巴拉巴拉……",
+  reading:"112",
+  collection:"96",
+  headImgsrc:"/images/2.jpg",
+  author:"黑衣人",
+  dateTime:"24小时前",
+},
+{ date:"Sep 18 2020",
+  title:"5555555",
+  imgSrc:"/images/2.jpg",
+  avatar:"/images/2.png",
+  content:"菊黄蟹正肥，巴拉巴拉巴拉……",
+  reading:"112",
+  collection:"96",
+  headImgsrc:"/images/2.jpg",
+  author:"黑衣人",
+  dateTime:"24小时前",
+}
+]
+
+
+// 导出
+export {
+  postList
+}
+
+// module.exports = {
+//   postList:local_database
+// }
+
+// this.setData 只有在page页面的js才能调用，其他js页面不行
+// data.js的数据要被导出 export，哪里需要数据就哪里导入 import
+// js模块的导入导出
